874e2200f5da59d097b71fe11292c95078831a19
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-03 12:11:51 +0530

    [ruchi]: implemented Multiple addressbook using collection

--- src/AddressBook1.java
+++ src/AddressBook1.java
@@ -1,7 +1,5 @@
 
-    import java.util.Iterator;
-    import java.util.LinkedList;
-    import java.util.Scanner;
+    import java.util.*;
 
     class AddressBookCollection{
         public String FirstName;
@@ -12,6 +10,7 @@
         public int ZipCode;
         public String PhoneNumber;
         public String Email;
+
 
         AddressBookCollection(String FirstName, String LastName, String Address, String City, String State, int ZipCode, String PhoneNumber, String Email)
         {
@@ -110,11 +109,21 @@
 
 
     public class AddressBook1 {
-
+        //        public LinkedHashMap<String, String> lhm = new LinkedHashMap<String, String>();
         Scanner sc = new Scanner(System.in);
         public int number = 0;
-        public LinkedList<AddressBookCollection> addressbook=new LinkedList<AddressBookCollection>();
-        
+        public LinkedList<AddressBookCollection> addressbook = new LinkedList<AddressBookCollection>();
+
+        public LinkedList<AddressBookCollection> collegeFriends = new LinkedList<AddressBookCollection>();
+        public LinkedList<AddressBookCollection> schoolFriends = new LinkedList<AddressBookCollection>();
+
+
+//        //public void createAdressBook(String name){
+//            LinkedHashMap<String, LinkedList> lhm = new LinkedHashMap<String, LinkedList>();
+//
+//            lhm.put("CommonAddressBook",addressbook);
+//
+//        }
 
         public void addContact() {
 
@@ -135,66 +144,167 @@
             System.out.println("Enter Email");
             String Email = sc.next();
 
-            AddressBookCollection addressBookCollection=new AddressBookCollection(FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,Email);
+            System.out.println("In which addressBook you want to add 1.Common Address book 2. School friends 3.College friends");
+            int i = sc.nextInt();
+            AddressBookCollection addressBookCollection = new AddressBookCollection(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+            if (i == 1) {
 
-            addressbook.add(addressBookCollection);
-
+                
+                addressbook.add(addressBookCollection);
+            } else if (i == 2) {
+                schoolFriends.add(addressBookCollection);
+            } else if (i == 3) {
+                collegeFriends.add(addressBookCollection);
+            }
 
 
         }
 
         public void displayContact() {
+            System.out.println("which addressBook you want to display 1.Common Address book 2. School friends 3.College friends");
+            int j = sc.nextInt();
+            if (j == 1) {
+                for (int i = 0; i < addressbook.size(); i++) {
+                    System.out.println(addressbook.get(i));
+                }
 
-            for (int i=0;i<addressbook.size();i++) {
-                System.out.println(addressbook.get(i));
+            } else if (j == 2) {
+                for (int i = 0; i < schoolFriends.size(); i++) {
+                    System.out.println(addressbook.get(i));
+                }
+
+            } else if (j == 3) {
+                for (int i = 0; i < collegeFriends.size(); i++) {
+                    System.out.println(addressbook.get(i));
+                }
+
             }
+
 
         }
 
         public void EditContact() {
+
+            System.out.println("In which addressBook you want to Edit 1.Common Address book 2. School friends 3.College friends");
+            int j = sc.nextInt();
             System.out.println("Enter contact no whose details you want to edit");
             String Phonenumberedit = sc.next();
 
-            for (int j = 0; j < addressbook.size(); j++) {
-              if (addressbook.get(j).PhoneNumber.equals(Phonenumberedit)) {
-                    System.out.println("Enter First name");
-                  addressbook.get(j).FirstName = sc.next();
-                    System.out.println("Enter Last name");
-                  addressbook.get(j).LastName = sc.next();
-                    System.out.println("Enter address");
-                  addressbook.get(j).Address = sc.next();
-                    System.out.println("Enter city");
-                  addressbook.get(j).City = sc.next();
-                    System.out.println("Enter state");
-                  addressbook.get(j).State = sc.next();
-                    System.out.println("Enter zip code");
-                  addressbook.get(j).ZipCode = sc.nextInt();
-                    System.out.println("Enter Email");
-                  addressbook.get(j).Email = sc.next();
+            if (j == 1) {
+                for (int k = 0; k < addressbook.size(); k++) {
+                    if (addressbook.get(k).PhoneNumber.equals(Phonenumberedit)) {
+                        System.out.println("Enter First name");
+                        addressbook.get(k).FirstName = sc.next();
+                        System.out.println("Enter Last name");
+                        addressbook.get(k).LastName = sc.next();
+                        System.out.println("Enter address");
+                        addressbook.get(k).Address = sc.next();
+                        System.out.println("Enter city");
+                        addressbook.get(k).City = sc.next();
+                        System.out.println("Enter state");
+                        addressbook.get(k).State = sc.next();
+                        System.out.println("Enter zip code");
+                        addressbook.get(k).ZipCode = sc.nextInt();
+                        System.out.println("Enter Email");
+                        addressbook.get(k).Email = sc.next();
 
 
+                    }
+                }
+
+            }
+
+            if (j == 2) {
+                for (int k = 0; k < schoolFriends.size(); k++) {
+                    if (schoolFriends.get(k).PhoneNumber.equals(Phonenumberedit)) {
+                        System.out.println("Enter First name");
+                        schoolFriends.get(k).FirstName = sc.next();
+                        System.out.println("Enter Last name");
+                        schoolFriends.get(k).LastName = sc.next();
+                        System.out.println("Enter address");
+                        schoolFriends.get(k).Address = sc.next();
+                        System.out.println("Enter city");
+                        schoolFriends.get(k).City = sc.next();
+                        System.out.println("Enter state");
+                        schoolFriends.get(k).State = sc.next();
+                        System.out.println("Enter zip code");
+                        schoolFriends.get(k).ZipCode = sc.nextInt();
+                        System.out.println("Enter Email");
+                        schoolFriends.get(k).Email = sc.next();
+
+
+                    }
                 }
             }
 
+            if (j == 3) {
+                for (int k = 0; k < collegeFriends.size(); k++) {
+                    if (collegeFriends.get(k).PhoneNumber.equals(Phonenumberedit)) {
+                        System.out.println("Enter First name");
+                        collegeFriends.get(k).FirstName = sc.next();
+                        System.out.println("Enter Last name");
+                        collegeFriends.get(k).LastName = sc.next();
+                        System.out.println("Enter address");
+                        collegeFriends.get(k).Address = sc.next();
+                        System.out.println("Enter city");
+                        collegeFriends.get(k).City = sc.next();
+                        System.out.println("Enter state");
+                        collegeFriends.get(k).State = sc.next();
+                        System.out.println("Enter zip code");
+                        collegeFriends.get(k).ZipCode = sc.nextInt();
+                        System.out.println("Enter Email");
+                        collegeFriends.get(k).Email = sc.next();
+
+
+                    }
+                }
+
+            }
         }
 
         public void DeleteRecord() {
+            System.out.println("In which addressBook you want to Edit 1.Common Address book 2. School friends 3.College friends");
+            int j = sc.nextInt();
             System.out.println("Enter name whose contact you want to delete");
             String firstname1 = sc.next();
-            Iterator itr=addressbook.iterator();
-            for(int i=0;i<addressbook.size();i++)
-            {
-                if (addressbook.get(i).FirstName.equals(firstname1))
-                {
-                    addressbook.remove(i);
-                   // addressbook.get(i).FirstName="aha";
+
+            if (j == 1) {
+                for (int i = 0; i < addressbook.size(); i++) {
+                    if (addressbook.get(i).FirstName.equals(firstname1)) {
+                        addressbook.remove(i);
+                        // addressbook.get(i).FirstName="aha";
+                    }
+
+
                 }
 
 
             }
+            if (j == 2) {
+                for (int i = 0; i < schoolFriends.size(); i++) {
+                    if (schoolFriends.get(i).FirstName.equals(firstname1)) {
+                        schoolFriends.remove(i);
+                        // addressbook.get(i).FirstName="aha";
+                    }
 
 
                 }
+
+
             }
+            if (j == 3) {
+                for (int i = 0; i < collegeFriends.size(); i++) {
+                    if (collegeFriends.get(i).FirstName.equals(firstname1)) {
+                        collegeFriends.remove(i);
+                        // addressbook.get(i).FirstName="aha";
+                    }
+
+
+                }
+
+
+            }
+        }
+    }
 
 
